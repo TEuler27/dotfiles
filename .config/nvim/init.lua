4b6fafeb6cde8d62fbe1f203577a26b787cd8a22
TEuler27 <edo.iscrizioni@rinonline.com>
2026-02-01 16:35:42 +0100

    Added markdown plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- Key Remapping
 vim.keymap.set('i', 'jj', '<Esc>', {})
+
+-- Plugins
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+
+Plug('OXY2DEV/markview.nvim')
+
+vim.call('plug#end')
