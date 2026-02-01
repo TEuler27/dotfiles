a32ebdc5a2e537906c3e5de899f324f7e0ba96a4
TEuler27 <edo.iscrizioni@rinonline.com>
2026-02-01 17:36:33 +0100

    Added Markview dependencies and basic setup.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,24 @@
 -- Key Remapping
 vim.keymap.set('i', 'jj', '<Esc>', {})
 
--- Plugins
+-- Plugins installation
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 
+Plug('nvim-mini/mini.icons')
+Plug('nvim-treesitter/nvim-treesitter')
 Plug('OXY2DEV/markview.nvim')
 
 vim.call('plug#end')
+
+-- Plugin loading with settings
+require('mini.icons').setup()
+require('markview').setup({
+	preview = {
+		enable = true,
+		enable_hybrid_mode = true,
+		modes = { "R", "i", "n", "no", "c" },
+		hybrid_modes = { "R", "i", "n", "no", "c" },
+	}
+})
+
