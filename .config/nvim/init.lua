0841d809dec1a026d02e48ea8fc3c6423bf21574
TEuler27 <edo.iscrizioni@rinonline.com>
2026-02-21 11:05:00 +0100

    Installed VimTex with some standard keybindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,6 +8,7 @@ vim.call('plug#begin')
 Plug('nvim-mini/mini.icons')
 Plug('nvim-treesitter/nvim-treesitter')
 Plug('OXY2DEV/markview.nvim')
+Plug('lervag/vimtex')
 Plug('EdenEast/nightfox.nvim') 
 
 vim.call('plug#end')
@@ -22,5 +23,22 @@ require('markview').setup({
 		hybrid_modes = { "v", "V", "R", "i", "n", "no", "c" },
 	}
 })
+require('nvim-treesitter.config').setup({
+	ignore_install = { "latex" }
+})
 vim.cmd("colorscheme nordfox")
 
+-- VimTex setting
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_quickfix_open_on_warning = 0
+vim.keymap.set('n', 'dsm', '<Plug>(vimtex-env-delete-math)', {})
+vim.keymap.set('n', 'csm', '<Plug>(vimtex-env-change-math)', {})
+vim.keymap.set('n', 'tsm', '<Plug>(vimtex-env-toggle-math)', {})
+vim.keymap.set('x', 'am', '<Plug>(vimtex-a$)', {})
+vim.keymap.set('x', 'im', '<Plug>(vimtex-i$)', {})
+vim.keymap.set('x', 'ai', '<Plug>(vimtex-am)', {})
+vim.keymap.set('x', 'ii', '<Plug>(vimtex-im)', {})
+vim.keymap.set('o', 'am', '<Plug>(vimtex-a$)', {})
+vim.keymap.set('o', 'im', '<Plug>(vimtex-i$)', {})
+vim.keymap.set('o', 'ai', '<Plug>(vimtex-am)', {})
+vim.keymap.set('o', 'ii', '<Plug>(vimtex-im)', {})
